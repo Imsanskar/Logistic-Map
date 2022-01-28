b977a0ef8d7c050704ac04d7b216418b817ff085
Sanskar Amgain <sanskaramgain@gmail.com>
2022-01-28 06:25:12 +0545

    multi threading added and guess what didn't improve shit

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,11 @@
 #include <GLFW/glfw3.h>
 #include "Math/matrix.h"
 #include "Math/vec.h"
+#include <sys/types.h>
 
-#define no_values 20000
-
+#define no_values 100000
+#define THREAD_NO 5
+//#define MULTI_THREAD
 MathLib::vec2 start, end;
 
 struct Point {
@@ -24,15 +26,131 @@ double bifurcation_value(double r, double xn) {
 	return xn;
 }
 
+struct BifurcateData{
+	double* y_values;
+	int start, end;
+};
 
-void bifurcation(double *y_value, int width, int height) {
+#if defined(__gnu_linux__) || defined(__linux__) || defined(linux) || defined(__linux)
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+// Declaration of thread condition variable
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+#endif
+
+
+#ifdef _MSC_VER
+#include <Windows.h>
+DWORD bifurcate_multithreading(void* param){
+	BifurcateData *thread_info = (BifurcateData *)param;
+	for(int i = thread_info->start; i < thread_info->end; i++){
+		float x_val = start.x + ((float)i / no_values) * (end.x - start.x);
+		double b_value = bifurcation_value(x_val, 0.4);
+#ifdef MULTI_THREAD
+		thread_info->y_values[i] = b_value;
+#else
+		thread_info->y_values[i] = b_value;
+#endif
+
+	}
+
+	return NULL;
+}
+
+void bifurcation(double* y_value, int width, int height) {
+#ifdef MULTI_THREAD
+	HANDLE threads[THREAD_NO];
+	BifurcateData data[THREAD_NO];
+
+
+	int start_index = 0;
+	const int partition_size = no_values / THREAD_NO;
+	int end_index = start_index + partition_size;
+	for (int i = 0; i < THREAD_NO && start_index < no_values && end_index <= no_values; i++) {
+		data[i] = BifurcateData{
+			y_value,
+			start_index,
+			end_index
+		};
+		start_index = end_index;
+		end_index += partition_size;
+		end_index = end_index < no_values ? end_index : no_values;
+		threads[i] = CreateThread(0, 0, bifurcate_multithreading, &data[i], 0, 0);
+	}
+
+	WaitForMultipleObjects(THREAD_NO, threads, 1, INFINITE);
+
+#else
 	for (int x = 0; x < no_values; x++) {
 		float x_val = start.x + ((float)x / no_values) * (end.x - start.x);
 
 		y_value[x] = bifurcation_value(x_val, 0.4);
 	}
-
+#endif
 }
+
+
+#endif // _MSC_VER
+
+#if defined(__gnu_linux__) || defined(__linux__) || defined(linux) || defined(__linux)
+#include <pthread.h>
+void* bifurcate_multithreading(void* param){
+	BifurcateData *thread_info = (BifurcateData *)param;
+	for(int i = thread_info->start; i < thread_info->end; i++){
+		float x_val = start.x + ((float)i / no_values) * (end.x - start.x);
+		double b_value = bifurcation_value(x_val, 0.4);
+		
+#if defined(MULTI_THREAD)
+		// lock mutex for data assignment
+		pthread_mutex_lock(&lock);
+		thread_info->y_values[i] = b_value;
+		pthread_mutex_unlock(&lock);
+#else
+		thread_info->y_values[i] = b_value;
+#endif
+
+	}
+
+	return NULL;
+}
+
+void bifurcation(double *y_value, int width, int height) {
+#ifdef MULTI_THREAD
+	HANDLE threads[THREAD_NO];
+	BifurcateData data[THREAD_NO];
+
+
+	uint start_index = 0;
+	const uint partition_size = no_values / THREAD_NO;
+	uint end_index = start_index + partition_size;
+	for(int i = 0; i < THREAD_NO && start_index < no_values && end_index <= no_values; i++){
+		data[i] = BifurcateData{
+			y_value,
+			start_index,
+			end_index
+		};
+		start_index = end_index;
+		end_index += partition_size;
+		end_index = end_index < no_values?end_index:no_values; 
+		pthread_create(&threads[i], NULL, bifurcate_multithreading, &data[i]);
+	}
+
+	for(int i = 0; i < THREAD_NO; i++){
+		pthread_join(threads[i], 0);
+	}
+
+#else
+	for (int x = 0; x < no_values; x++) {
+		float x_val = start.x + ((float)x / no_values) * (end.x - start.x);
+
+		y_value[x] = bifurcation_value(x_val, 0.4);
+	}
+#endif
+}
+#endif
+
+
+
+
 
 inline float MapRange(float from_x1, float from_x2, float to_x1, float to_x2, float x) {
 	return (to_x2 - to_x1) / (from_x2 - from_x1) * (x - from_x1) + to_x1;
@@ -49,7 +167,7 @@ void render_logistic_map(double* y_values, int width, int height) {
 
 		double y_value = y_values[x];
 		glVertex2f(MapRange(start.x, end.x, 0, (float)width, x_val), MapRange(start.y, end.y, 0, height, y_value));
-		for(int i = 0; i < 50; i++){
+		for(int i = 0; i < 20; i++){
 			y_value = x_val * y_value * (1 - y_value); 
 			glVertex2f(MapRange(start.x, end.x, 0, (float)width, x_val), MapRange(start.y, end.y, 0, height, y_value));
 		}
@@ -155,22 +273,39 @@ int main() {
 	end = rectMax;
 
 	glfwMakeContextCurrent(window);
-	glViewport(0, 0, width, height);
+	// glViewport(0, 0, width, height);
 
 	double* y_values = (double *)malloc(sizeof(double) * no_values);
 	screen.y_value = y_values;
 	bifurcation(y_values, width, height);
 	glfwSetWindowUserPointer(window, &screen);
 
+	double previousTime = glfwGetTime();
+	int frameCount = 0;
+
 
 	while (!glfwWindowShouldClose(window)){
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		render_logistic_map(y_values, screen.width, screen.height);
 
-		processKeyboardInput(window);
 		glfwSwapBuffers(window);
+
+		// Measure speed
+		double currentTime = glfwGetTime();
+		frameCount++;
+		if (currentTime - previousTime > 1.0){
+			// If a second has passed.
+			// Display the frame count here any way you want
+			printf("Frame Count%d\n", frameCount);
+
+			frameCount = 0;
+			previousTime = currentTime;
+		}
+
+
 		glfwPollEvents();
+		processKeyboardInput(window);
 	}
 }
